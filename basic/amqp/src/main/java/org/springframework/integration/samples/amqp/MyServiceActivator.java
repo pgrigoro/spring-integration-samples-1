c13be2727088c3031ee3a8cb96e6423ed2dbdc3e
Yosep Kim <yk@venturebang.com>
2017-07-31 23:48:28 -0400

    errorChannel is a special channel that is already a publish subscribe channel

--- basic/amqp/src/main/java/org/springframework/integration/samples/amqp/MyServiceActivator.java
+++ basic/amqp/src/main/java/org/springframework/integration/samples/amqp/MyServiceActivator.java
@@ -3,19 +3,19 @@ package org.springframework.integration.samples.amqp;
 import org.springframework.amqp.core.Message;
 
 public class MyServiceActivator {
-    public Message handleMessage1(Exception message) {
+    public void handleMessage1(Exception message) {
         System.out.println("handleMessage1");
-        return null;
+        //return null;
     }
 
-    public Message handleMessage2(Exception message) {
+    public void handleMessage2(Exception message) {
         System.out.println("handleMessage2");
-        return null;
+        //return null;
     }
 
-    public String handleMessage3(Exception message) {
+    public void handleMessage3(Exception message) {
         System.out.println("handleMessage3: " + message.getMessage());
-        return message.getMessage();
+        //return message.getMessage();
     }
 
 
