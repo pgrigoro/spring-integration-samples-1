f6710a12a4ab49633a182bee4c1363764f569277
Yosep Kim <yk@venturebang.com>
2017-08-04 00:42:59 -0400

    Default errorChannel runs in a single thread for the synchronous gateway

--- basic/ws-inbound-gateway/src/main/java/org/springframework/integration/samples/ws/ErrorHandler.java
+++ basic/ws-inbound-gateway/src/main/java/org/springframework/integration/samples/ws/ErrorHandler.java
@@ -4,7 +4,7 @@ public class ErrorHandler {
 
     public String handleError(Exception ex) throws InterruptedException {
         System.out.println(Thread.currentThread().getId() + " - Started Handling Error");
-        Thread.sleep(100); // Start breaking down when it reaches 997
+        Thread.sleep(5000); // Start breaking down when it reaches 997
 //        System.out.println(Thread.currentThread().getId() + " - Woke up from a 5 second sleep in the Error Handler");
         return "<echoResponse xmlns=\"http://www.springframework.org/spring-ws/samples/echo\">I am alive</echoResponse>";
     }
