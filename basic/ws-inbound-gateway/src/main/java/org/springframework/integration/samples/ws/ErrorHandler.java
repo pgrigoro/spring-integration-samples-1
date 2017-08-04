1e728ede02fcee1b49768ff48d50acd86bff37a3
Yosep Kim <yk@venturebang.com>
2017-08-03 21:27:09 -0400

    Error handler works fine even after a long sleep, if it's not excuted through an executor

--- basic/ws-inbound-gateway/src/main/java/org/springframework/integration/samples/ws/ErrorHandler.java
+++ basic/ws-inbound-gateway/src/main/java/org/springframework/integration/samples/ws/ErrorHandler.java
@@ -4,8 +4,8 @@ public class ErrorHandler {
 
     public String handleError(Exception ex) throws InterruptedException {
         System.out.println("Started Handling Error");
-        Thread.sleep(3000); // Start breaking down when it reaches 997
-        System.out.println("Woke up from a long sleep in the Error Handler");
+        Thread.sleep(5000); // Start breaking down when it reaches 997
+        System.out.println("Woke up from a 5 second sleep in the Error Handler");
         return "<echoResponse xmlns=\"http://www.springframework.org/spring-ws/samples/echo\">I am alive</echoResponse>";
     }
 }
