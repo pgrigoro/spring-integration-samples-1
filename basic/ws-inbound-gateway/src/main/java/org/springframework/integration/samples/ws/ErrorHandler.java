190a342d45c45425ef4c229a500a31c888ab8ae8
Yosep Kim <yk@venturebang.com>
2017-08-03 21:24:38 -0400

    Error handler starts failing, when it waits too long, i.e. more than 997 milliseconds

--- basic/ws-inbound-gateway/src/main/java/org/springframework/integration/samples/ws/ErrorHandler.java
+++ basic/ws-inbound-gateway/src/main/java/org/springframework/integration/samples/ws/ErrorHandler.java
@@ -4,7 +4,7 @@ public class ErrorHandler {
 
     public String handleError(Exception ex) throws InterruptedException {
         System.out.println("Started Handling Error");
-        Thread.sleep(100); // Start breaking down when it reaches 997
+        Thread.sleep(3000); // Start breaking down when it reaches 997
         System.out.println("Woke up from a long sleep in the Error Handler");
         return "<echoResponse xmlns=\"http://www.springframework.org/spring-ws/samples/echo\">I am alive</echoResponse>";
     }
